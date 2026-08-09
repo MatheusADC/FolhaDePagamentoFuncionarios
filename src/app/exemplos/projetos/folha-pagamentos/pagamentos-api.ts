787c66771bbeb27b412f36f7a44a7741c629a297
Matheus Amaral <161625293+MatheusADC@users.noreply.github.com>
2026-08-09 20:54:44 -0300

    ✨ feat: Método de retorno dos funcionários

--- src/app/exemplos/projetos/folha-pagamentos/pagamentos-api.ts
+++ src/app/exemplos/projetos/folha-pagamentos/pagamentos-api.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Observable, of } from "rxjs";
+import { delay, map, Observable, of } from "rxjs";
 
 export interface IFuncionario {
     id: number;
@@ -13,7 +13,22 @@ export interface IFuncionario {
 })
 export class PagamentosApi {
     getFuncionarios(): Observable<IFuncionario[]> {
-        return of([]);
+      // Número entre 1000ms e 5000ms
+      const tempoAleatorio = Math.floor(Math.random() * 4000) + 1000;
+
+        return of([
+          { id: 0, nome: 'Ana Clara' },
+          { id: 1, nome: 'Luiz Carlos' },
+          { id: 2, nome: 'Rodrigo Silva' },
+          { id: 3, nome: 'Ricardo Alves' },
+        ]).pipe(
+          delay(tempoAleatorio),
+          map((funcionarios) => {
+            return funcionarios.map(
+              (funcionario) => ({... funcionario, selecionado: true, status: 'pendente' } as IFuncionario)
+            )
+          })
+        );
     }
 
     pagarFuncionario() {
