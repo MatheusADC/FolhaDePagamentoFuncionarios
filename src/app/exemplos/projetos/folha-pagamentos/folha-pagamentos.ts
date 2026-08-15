291a8aa77e758b048d00f9a8318cfbe4fcae0f65
Matheus Amaral <161625293+MatheusADC@users.noreply.github.com>
2026-08-15 19:26:00 -0300

    🧪 test: Teste do pagamento para um funcionário específico

--- src/app/exemplos/projetos/folha-pagamentos/folha-pagamentos.ts
+++ src/app/exemplos/projetos/folha-pagamentos/folha-pagamentos.ts
@@ -1,5 +1,6 @@
 import { Component, computed, inject, signal } from '@angular/core';
 import { IFuncionario, PagamentosApi } from './pagamentos-api';
+import { filter, map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-folha-pagamentos',
@@ -20,9 +21,20 @@ export class FolhaPagamentos {
   );
 
   ngOnInit() {
-    this._pagamentosApi.getFuncionarios().subscribe(
-      (funcionariosResponse) => console.log('Funcionários: ', funcionariosResponse)
-    );
+    // this._pagamentosApi.getFuncionarios().subscribe(
+    //   (funcionariosResponse) => console.log('Funcionários: ', funcionariosResponse)
+    // );
+
+    // Teste de pagamento de um único funcionário
+    this._pagamentosApi.getFuncionarios().pipe(
+      map((funcionariosResponse) => funcionariosResponse.find((f) => f.id === 2)),
+      filter((funcionario) => funcionario !== undefined),
+      switchMap((funcionario) => this._pagamentosApi.pagarFuncionario(funcionario)),
+    )
+    .subscribe({
+      next: (response) => console.log('Pagamento: ', response),
+      error: (erro) => console.log('Erro no pagamento: ', erro),
+    });
   }
 
   toggleSelecao(id: number) {
