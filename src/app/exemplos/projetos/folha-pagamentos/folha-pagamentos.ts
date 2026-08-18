785d54eec3d82508a7d569f83f328373e5957722
Matheus Amaral <161625293+MatheusADC@users.noreply.github.com>
2026-08-18 15:21:29 -0300

    ✨ feat: Lógica de listagem dos funcionários

--- src/app/exemplos/projetos/folha-pagamentos/folha-pagamentos.ts
+++ src/app/exemplos/projetos/folha-pagamentos/folha-pagamentos.ts
@@ -1,6 +1,6 @@
 import { Component, computed, inject, signal } from '@angular/core';
 import { IFuncionario, PagamentosApi } from './pagamentos-api';
-import { catchError, concatMap, filter, from, map, of, switchMap, tap } from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-folha-pagamentos',
@@ -11,42 +11,31 @@ import { catchError, concatMap, filter, from, map, of, switchMap, tap } from 'rx
 export class FolhaPagamentos {
   private readonly _pagamentosApi = inject(PagamentosApi);
 
-  funcionarios = signal<IFuncionario[]>([]);
-
   consoleLogs = signal<string[]>(['Sistema pronto para iniciar.']);
   processando = signal(false);
 
-  funcionariosSelecionados = computed(() =>
-    this.funcionarios().filter(f => f.selecionado)
-  );
+  funcionariosResource = rxResource({
+    params: () => true,
+    stream: () => this._pagamentosApi.getFuncionarios(),
+  });
 
-  ngOnInit() {
-    // this._pagamentosApi.getFuncionarios().subscribe(
-    //   (funcionariosResponse) => console.log('Funcionários: ', funcionariosResponse)
-    // );
+  funcionarios = computed(() => {
+    console.log('Funcionários Resource: ', this.funcionariosResource.hasValue());
 
-    // Teste de pagamento de um único funcionário
-    // this._pagamentosApi.getFuncionarios().pipe(
-    //   map((funcionariosResponse) => funcionariosResponse.find((f) => f.id === 2)),
-    //   filter((funcionario) => funcionario !== undefined),
-    //   switchMap((funcionario) => this._pagamentosApi.pagarFuncionario(funcionario)),
-    // )
-    // .subscribe({
-    //   next: (response) => console.log('Pagamento: ', response),
-    //   error: (erro) => console.log('Erro no pagamento: ', erro),
-    // });
+    if (this.funcionariosResource.hasValue()) {
+      return this.funcionariosResource.value();
+    }
 
-    // Teste do pagamento de todos os funcionários
-    this._pagamentosApi.getFuncionarios().pipe(
-      tap((funcionarios) => console.log('Lista de funcionários: ', funcionarios)),
-      switchMap((funcionarios) => from(funcionarios)),
-      tap((funcionario) => console.log('Funcionário atual: ', funcionario)),
-      concatMap((funcionario) => this._pagamentosApi.pagarFuncionario(funcionario).pipe(
-        catchError((erroPagamento) => of(erroPagamento))
-      )),
-    )
-      .subscribe((pagamentoResponse) => console.log('Pagemento: ', pagamentoResponse));
-  }
+    return [];
+  });
+
+  funcionariosSelecionados = computed(() => {
+    if (this.funcionariosResource.hasValue()) {
+      return this.funcionariosResource.value().filter(f => f.selecionado);
+    }
+
+    return [];
+  });
 
   toggleSelecao(id: number) {
 
